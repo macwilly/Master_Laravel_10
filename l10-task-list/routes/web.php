081fbb712bad88f467bbd5298c20107a76508806
mackenzie willard <mtwillard29@gmail.com>
2023-12-03 17:49:54 -0500

    Some Route clean up
    Adding on /tasks to the tasks Route.
    Redirecting landing Route to go to the tasks.index Route

--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -80,15 +80,18 @@ $tasks = [
     ),
 ];
 
+Route::get('/', function (){
+   return redirect()->route('tasks.index');
+});
 
 //Dynamically taking the name from the URL argument
-Route::get('/', function () use ($tasks) {
+Route::get('/tasks', function () use ($tasks) {
     return view('index', [
          "tasks"=>$tasks
     ]);
 })->name('tasks.index');
 
-Route::get('/{id}', function ($id){
+Route::get('/tasks/{id}', function ($id){
     return 'One single task';
 })->name('tasks.show');
 
