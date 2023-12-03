9114f861a61e71f51a76008b6cc8a19ce9083da9
mackenzie willard <mtwillard29@gmail.com>
2023-12-03 18:13:35 -0500

    Setting up a basic template

---
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+    <title>Laravel 10 List App</title>
+    </head>
+    <body>
+        <h1></h1>
+        <div>
+
+        </div>
+    </body>
+</html>
