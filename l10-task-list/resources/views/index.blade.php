cf20058efb71b5549419cc1b11c3c793d6f82b33
mackenzie willard <mtwillard29@gmail.com>
2023-12-03 17:39:32 -0500

    Displaying tasks as a link
    Link connects to the new Route to display a task. View comment for how the Routing working in the link.

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -5,7 +5,10 @@
 <div>
 
         @forelse($tasks as $task)
-          <div>{{$task->title}}</div>
+            <div>
+                {{-- passing in the route function with the name of the route, as well as blade array with the id--}}
+                <a href="{{ route('tasks.show', ['id'=>$task->id]) }}">{{ $task->title }}</a>
+            </div>
         @empty
             <div>There are no tasks</div>
        @endforelse
