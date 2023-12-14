829f1daac56a6929bab97a1be35ac631fd4425e2
mackenzie willard <mtwillard29@gmail.com>
2023-12-14 16:39:28 -0500

    Keeping good data
    adding old('[inputName]') to the input fields

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -21,7 +21,7 @@
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title" value="{{ old('title') }}"/>
             @error('title')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -30,7 +30,7 @@
             <label for="description">
                 Description
             </label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
             @error('description')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -39,7 +39,7 @@
             <label for="long_description">
                 Long Description
             </label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
             <p class="error-message">{{ $message }}</p>
             @enderror
