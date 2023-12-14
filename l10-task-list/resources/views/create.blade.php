74fb7d0cdda2e7fe051d1637da6416bc316ef78d
mackenzie willard <mtwillard29@gmail.com>
2023-12-13 20:06:07 -0500

    Using Error directive
    this directive will allow the access of the error session variable to to use the field name to get the message.

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -10,18 +10,27 @@
                 Title
             </label>
             <input type="text" name="title" id="title" />
+            @error('title')
+            <p>{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="description">
                 Description
             </label>
             <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+            <p>{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="long_description">
                 Long Description
             </label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+            <p>{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <button type="submit">Add Task</button>
