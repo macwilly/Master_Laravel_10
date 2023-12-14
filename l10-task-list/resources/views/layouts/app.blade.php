098dab210881b1c784bd408606fccdd0e8718f9d
mackenzie willard <mtwillard29@gmail.com>
2023-12-14 16:00:52 -0500

    Displaying the success message
    When the data was entered into the database the success message will be displayed one time.

--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <body>
         <h1>@yield('title')</h1>
         <div>
+            {{-- Checking to see is the session has the success field  --}}
+            @if(session()->has('success'))
+                {{-- Desiplaying the session['success'] message that was set when data was inserted into the database --}}
+                <div>{{ session('success') }}</div>
+            @endif
             @yield('content')
         </div>
     </body>
