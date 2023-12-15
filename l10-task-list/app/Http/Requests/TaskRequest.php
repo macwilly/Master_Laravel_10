300beacdf70a9cd99c312b955efb58b26742757a
mackenzie willard <mtwillard29@gmail.com>
2023-12-15 17:33:16 -0500

    Moving Validation
    Moving the validation from the wed.app to rules() function

--- l10-task-list/app/Http/Requests/TaskRequest.php
+++ l10-task-list/app/Http/Requests/TaskRequest.php
@@ -22,7 +22,9 @@ class TaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'            => 'required|max:255',
+            'description'      => 'required',
+            'long_description' => 'required'
         ];
     }
 }
