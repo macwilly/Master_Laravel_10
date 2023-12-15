34fddc559e57f24a40a770ead9070d715a1babef
mackenzie willard <mtwillard29@gmail.com>
2023-12-15 16:19:43 -0500

    Changing the Parameter names
    Updating the parameter names from Id to Task to match refactoring.

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -9,7 +9,7 @@
     @forelse($tasks as $task)
         <div>
             {{-- passing in the route function with the name of the route, as well as blade array with the id--}}
-            <a href="{{ route('tasks.show', ['id'=>$task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task'=>$task->id]) }}">{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks</div>
