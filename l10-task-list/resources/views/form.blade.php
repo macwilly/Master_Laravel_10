7cfdb089c89e9b702330517c8c214dadbd0cdfd6
mackenzie willard <mtwillard29@gmail.com>
2023-12-15 20:07:04 -0500

    Null checks on task object variables
    This is using a ?? to keep it clean

--- l10-task-list/resources/views/form.blade.php
+++ l10-task-list/resources/views/form.blade.php
@@ -24,7 +24,7 @@
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}"/>
+            <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}"/>
             @error('title')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -33,7 +33,8 @@
             <label for="description">
                 Description
             </label>
-            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
+            <textarea name="description" id="description"
+                      rows="5">{{ $task->description ?? old('description') }}</textarea>
             @error('description')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -42,7 +43,8 @@
             <label for="long_description">
                 Long Description
             </label>
-            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
+            <textarea name="long_description" id="long_description"
+                      rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
             @error('long_description')
             <p class="error-message">{{ $message }}</p>
             @enderror
